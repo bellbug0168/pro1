d66eb1688bb6b308d5ae0e9b2f36ad4c1d617ae9
hong <hong@naver.com>
2019-06-19 16:24:06 +0900

    sample class 추가

--- src/fvsdsf/qwfqwf.java
+++ src/fvsdsf/qwfqwf.java
@@ -1,5 +1,20 @@
 package fvsdsf;
 
 public class qwfqwf {
-
+	private String name;
+	private int num;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
 }
